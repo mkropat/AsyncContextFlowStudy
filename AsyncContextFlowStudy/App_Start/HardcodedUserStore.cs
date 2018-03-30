5524b66e473fc90e7bfe09f5be52ac1e2244265c
Michael Kropat <michael.kropat@rackspace.com>
2018-03-30 14:43:05 -0400

    Replace user store with hardcoded users

---
+++ AsyncContextFlowStudy/App_Start/HardcodedUserStore.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncContextFlowStudy.Models;
+using Microsoft.AspNet.Identity;
+
+namespace AsyncContextFlowStudy.App_Start
+{
+    public sealed class HardcodedUserStore : IUserStore<ApplicationUser>, IUserPasswordStore<ApplicationUser>, IUserLockoutStore<ApplicationUser, string>, IUserTwoFactorStore<ApplicationUser, string>
+    {
+        readonly ApplicationUser[] _users;
+        public HardcodedUserStore(IEnumerable<ApplicationUser> users)
+        {
+            _users = users.ToArray();
+        }
+
+        public Task CreateAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public Task<ApplicationUser> FindByIdAsync(string userId)
+        {
+            return Task.FromResult(_users.SingleOrDefault(u => u.Id == userId));
+        }
+
+        public Task<ApplicationUser> FindByNameAsync(string userName)
+        {
+            return Task.FromResult(_users.SingleOrDefault(u => u.Email == userName));
+        }
+
+        public Task UpdateAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DateTimeOffset> GetLockoutEndDateAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetLockoutEndDateAsync(ApplicationUser user, DateTimeOffset lockoutEnd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> IncrementAccessFailedCountAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ResetAccessFailedCountAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetAccessFailedCountAsync(ApplicationUser user)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task<bool> GetLockoutEnabledAsync(ApplicationUser user)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task SetLockoutEnabledAsync(ApplicationUser user, bool enabled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(ApplicationUser user, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(ApplicationUser user)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(ApplicationUser user)
+        {
+            return Task.FromResult(!string.IsNullOrEmpty(user.PasswordHash));
+        }
+
+        public Task SetTwoFactorEnabledAsync(ApplicationUser user, bool enabled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetTwoFactorEnabledAsync(ApplicationUser user)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
--- AsyncContextFlowStudy/App_Start/Startup.Auth.cs
+++ AsyncContextFlowStudy/App_Start/Startup.Auth.cs
@@ -1,11 +1,10 @@
 ﻿using System;
+using AsyncContextFlowStudy.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
-using AsyncContextFlowStudy.Models;
 
 namespace AsyncContextFlowStudy
 {
@@ -14,8 +13,6 @@ namespace AsyncContextFlowStudy
         // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
