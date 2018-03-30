4e9ba5c13f0a61b94eb17fe34b6ef6e92b06b2f7
Michael Kropat <michael.kropat@rackspace.com>
2018-03-30 13:56:46 -0400

    New project

---
+++ AsyncContextFlowStudy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AsyncContextFlowStudy.Startup))]
+namespace AsyncContextFlowStudy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
